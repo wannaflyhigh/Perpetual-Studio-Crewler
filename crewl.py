60a5747c05df3534e9bc98113ec723cc493c274f
wannaflyhigh <joeydai901108@gmail.com>
2023-05-19 01:05:58 +0800

    新增部分型別

--- crewl.py
+++ crewl.py
@@ -1,96 +1,153 @@
+from typing import Dict, List
+from dataclasses import dataclass
 import requests as req
 import bs4
 
-def simpleQuery(params={}, url=''):
-	res = ''
-	if (url != ''):
-		res = req.get(url)
-	else:
-		postUrl = "http://www.lib.ntnu.edu.tw/holding/doQuickSearch.jsp"
-		res = req.post(postUrl, params)
-	table = bs4.BeautifulSoup(res.text,
-							  features="html.parser").find_all('table')
-	aTags = table[0].find_all('a')
 
-	bookTypes = []
+@dataclass
+class bookType:
+    name: str
+    href: str
 
-	for aTag in aTags:
-		name = str(aTag.contents[0]).strip()
-		href = 'http://www.lib.ntnu.edu.tw/holding/' + \
-			str(aTag.get('href')).replace("¶", "&para")
-		bookTypes.append({'name': name, 'href': href})
+    def __init__(self, name: str, href: str):
+        self.name = name
+        self.href = href
 
-	pages = bs4.BeautifulSoup(res.text, features="html.parser").find_all(
-		'div', {'class': 'jumppage'})[0].find_all('a')
-	pageLinks = []
-	for page in pages:
-		name = str(page.contents[0]).strip()
-		href = 'http://www.lib.ntnu.edu.tw/holding/' + \
-			str(page.get('href')).replace("¶", "&para")
-		pageLinks.append({'name': name, 'href': href})
 
-	result = {'bookTypes': bookTypes, 'pageLinks': pageLinks}
-	return result
+@dataclass
+class pageLink:
+    name: str
+    href: str
 
-def singleBookOrMulti(options):
-	aS = options[0].find_all('a')
-	for a in aS:
-		if(a.contents[0] == '顯示MARC'):
-			return 'single'
-	return 'multi'
+    def __init__(self, name: str, href: str):
+        self.name = name
+        self.href = href
+
+
+@dataclass
+class queryReturnType:
+    bookTypes: List[bookType]
+    pageLinks: List[pageLink]
+
+    def __init__(self, bookTypes: List[bookType], pageLinks: List[pageLink]):
+        self.bookTypes = bookTypes
+        self.pageLinks = pageLinks
+
+
+def simpleQuery(params: dict = {}, url='') -> queryReturnType:
+    res = ''
+    if (url != ''):
+        res = req.get(url)
+    else:
+        postUrl = "http://www.lib.ntnu.edu.tw/holding/doQuickSearch.jsp"
+        res = req.post(postUrl, params)
+    table = bs4.BeautifulSoup(res.text,
+                              features="html.parser").find_all('table')
+    aTags = table[0].find_all('a')
+
+    bookTypes: List[bookType] = []
+
+    for aTag in aTags:
+        name = str(aTag.contents[0]).strip()
+        href = 'http://www.lib.ntnu.edu.tw/holding/' + \
+         str(aTag.get('href')).replace("¶", "&para")
+        bookTypes.append(bookType(name=name, href=href))
+
+    pages = bs4.BeautifulSoup(res.text, features="html.parser").find_all(
+        'div', {'class': 'jumppage'})[0].find_all('a')
+    pageLinks: List[pageLink] = []
+    for page in pages:
+        name = str(page.contents[0]).strip()
+        href = 'http://www.lib.ntnu.edu.tw/holding/' + \
+         str(page.get('href')).replace("¶", "&para")
+        pageLinks.append(pageLink(name, href))
+
+    result = queryReturnType(bookTypes, pageLinks)
+    return result
+
+
+def singleBookOnly(options):
+    aS = options[0].find_all('a')
+    for a in aS:
+        if (a.contents[0] == '顯示MARC'):
+            return True
+    return False
+
 
 def findBook(url):
-	res = req.get(url)
-	options = bs4.BeautifulSoup(res.text,features="html.parser").find_all('div', {"class": "options"})
-	print(singleBookOrMulti(options)=='single')
-	if(singleBookOrMulti(options)=='single'):
-		return singleBook(res.text)
-	else:
-		return multiBook(res.text)
+    res = req.get(url)
+    options = bs4.BeautifulSoup(res.text, features="html.parser").find_all(
+        'div', {"class": "options"})
+    print(singleBookOnly(options))
+    if (singleBookOnly(options) == True):
+        return [{'detail': singleBook(res.text)}]
+    else:
+        return multiBook(res.text)
 
 
 def multiBook(text):
-	books = []
-	bookNameAndHrefs = bs4.BeautifulSoup(text,features="html.parser").find_all(
-		'tr', {"valign": "top"})
-	for index, bookNameAndHref in enumerate(bookNameAndHrefs):
-		bookDetail = {}
-		href = ''
-		name = ''
-		a = bookNameAndHref.find_all('a', href=True)
-		i = 0
-		if (a[0].contents[0] == '預約'):
-			i = 1
-		href = 'http://www.lib.ntnu.edu.tw/holding/' + \
-			str(a[i].get('href')).replace("¶", "&para")
-		name = a[i].find_all('span')[0].contents[0]
-		books.append({'href': href, 'name': name})
-	tables = bs4.BeautifulSoup(text,features="html.parser").find_all('table',
-												  {'class': 'width100'})
-	for index, table in enumerate(tables):
-		bookDetails = []
-		for i, td in enumerate(table.find_all('tr')[1].find_all('td')):
-			if (i == 4):
-				break
-			detail = td.contents[0]
-			if (i == 1 or i == 2):
-				detail = td.find_all('a')[0].contents[0]
-			bookDetails.append(detail)
-		books[index]['detail'] = bookDetails
-	return books
+    books = []
+    bookNameAndHrefs = bs4.BeautifulSoup(text,
+                                         features="html.parser").find_all(
+                                             'tr', {"valign": "top"})
+    for index, bookNameAndHref in enumerate(bookNameAndHrefs):
+        bookDetail = {}
+        href = ''
+        name = ''
+        a = bookNameAndHref.find_all('a', href=True)
+        i = 0
+        if (a[0].contents[0] == '預約'):
+            i = 1
+        href = 'http://www.lib.ntnu.edu.tw/holding/' + \
+         str(a[i].get('href')).replace("¶", "&para")
+        name = a[i].find_all('span')[0].contents[0]
+        books.append({'href': href, 'name': name})
+    tables = bs4.BeautifulSoup(text, features="html.parser").find_all(
+        'table', {'class': 'width100'})
+    for index, table in enumerate(tables):
+        bookDetails = []
+        for i, td in enumerate(table.find_all('tr')[1].find_all('td')):
+            if (i == 4):
+                break
+            detail = td.contents[0]
+            if (i == 1 or i == 2):
+                detail = td.find_all('a')[0].contents[0]
+            bookDetails.append(detail)
+        books[index]['detail'] = bookDetails
+    return books
+
 
 def singleBook(text):
-	table = bs4.BeautifulSoup(text,features="html.parser").find_all('table',
-												  {'class': 'collection_info'})[0]
-	bookDetails = []
-	for i, td in enumerate(table.find_all('tr')[1].find_all('td')):
-		if (i == 4):
-			break
-		detail = td.contents[0]
-		if (i == 1 or i == 2):
-			detail = td.find_all('a')[0].contents[0]
-		bookDetails.append(detail)
-	return bookDetails
+    table = bs4.BeautifulSoup(text, features="html.parser").find_all(
+        'table', {'class': 'collection_info'})[0]
+    bookDetails = []
+    for i, td in enumerate(table.find_all('tr')[1].find_all('td')):
+        if (i == 4):
+            break
+        detail = td.contents[0]
+        if (i == 1 or i == 2):
+            detail = td.find_all('a')[0].contents[0]
+        bookDetails.append(detail)
+    return bookDetails
+
+
+def correctWords(text):
+    res = req.get(
+        f'https://zhuan-ti-hou-duan.onrender.com/correctWords?text={text}')
+    resText = res.text
+    s = str(
+        bs4.BeautifulSoup(resText,
+                          features="html.parser").find_all('font')[0].text)
+    left_bracket = s.find('{')
+    right_bracket = s.find('}')
+    greater_than = s.find('>')
+    # print(s[:left_bracket]+s[greater_than+1:right_bracket]+s[right_bracket+1:])
+    if left_bracket != -1 and right_bracket != -1 and left_bracket < right_bracket:
+        s = s[:left_bracket] + s[greater_than +
+                                 1:right_bracket] + s[right_bracket + 1:]
+    print(s)
+    return s
+
 
 # def test():
 # 	url = "http://www.lib.ntnu.edu.tw/holding/doQuickSearch.jsp?action=view&param=%2Fsearch*cht%3F%2Ft%7Bu6F14%7D%7Bu7B97%7D%7Bu6CD5%7D%2Ft%7B214852%7D%7B21502c%7D%7B21472a%7D%2F1%252C24%252C31%252CB%2Fexact%26FF%3Dt%7B214852%7D%7B21502c%7D%7B21472a%7D%261%252C4%252C"
--- main.py
+++ main.py
@@ -1,41 +1,103 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from typing import Dict, List
 import crewl
 import json
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-	return {"msg": "早安"}
+    return {"msg": "早安"}
+
 
 @app.get("/bookPage")
-async def booksPage(urlObj:str):
-	url=json.loads(urlObj).get('url')
-	print(url)
-	result = crewl.simpleQuery(url=url)
-	return result
+async def bookPage(urlObj: str) -> crewl.queryReturnType:
+    print(urlObj)
+    url = json.loads(urlObj).get('url')
+    print(url)
+    result: crewl.queryReturnType = crewl.simpleQuery(url=url)
+    return result
+
 
 @app.get("/query")
 async def query(searchtype: str = 'all',
-					search: str = '',
-					title: str = '',
-					author: str = '',
-					url: str = ''):
-	result = ''
-	print(search)
-	result = crewl.simpleQuery(params={
-		'newQuery': 'true',
-		'searchtype': searchtype,
-		'search': search,
-		'title': title,
-		'author': author
-	})
-	print(result)
-	return {"result":result}
+                search: str = '',
+                title: str = '',
+                author: str = '',
+                url: str = '') -> crewl.queryReturnType:
+    print(search)
+    result: crewl.queryReturnType = crewl.simpleQuery(
+        params={
+            'newQuery': 'true',
+            'searchtype': searchtype,
+            'search': search,
+            'title': title,
+            'author': author
+        })
+    # print(result)
+    return result
+
 
 @app.get("/findBook")
-async def findBook(urlObj:str):
-	url=json.loads(urlObj).get('url')
-	# print(url)
-	result = crewl.findBook(url)
-	return result
+async def findBook(urlObj: str):
+    '''
+    # Parameters
+    ```javascript
+    urlObj = JSON.stringify({ url: url })
+    ```
+    ```python
+    urlObj = json.dumps({'url':url})
+    ```
+
+    
+    # Responses 
+    ## single book:
+
+    ```json
+    {'position', 'call_number', 'barcode', 'status'}
+
+    [ 
+        { 
+            detail: [ '公館分館6F新類號專區', '312.32C 8866-6', 'BM0789936', '可外借' ] 
+        } 
+    ]
+    ```
+
+    ## multi books:
+
+    ```json
+    [
+        {
+            href: 'http://www.lib.ntnu.edu.tw/holding/doQuickSearch.jsp?action=view&param=%2Fsearch*cht%3F%2Ft%257Bu6F14%257D%257Bu7B97%257D%257Bu6CD5%257D%2Ft%257B214852%257D%257B21502c%257D%257B21472a%257D%2F1%252C24%252C31%252CB%2Fframeset%26FF%3Dt%7B214852%7D%7B21502c%7D%7B21472a%7D%261%252C%252C4',
+            name: '圖說演算法 : 使用C# : 理解零負擔.採功能強大C#語言實作 / 吳燦銘, 胡昭民著',
+            detail: [ '公館分館6F新類號專區', '312.32C 8866-6', 'BM0789936', '可外借' ]
+        },
+        {
+            href: 'http://www.lib.ntnu.edu.tw/holding/doQuickSearch.jsp?action=view&param=%2Fsearch*cht%3F%2Ft%257Bu6F14%257D%257Bu7B97%257D%257Bu6CD5%257D%2Ft%257B214852%257D%257B21502c%257D%257B21472a%257D%2F1%252C24%252C31%252CB%2Fframeset%26FF%3Dt%7B214852%7D%7B21502c%7D%7B21472a%7D%262%252C%252C4',
+            name: '圖說演算法 : 使用C語言 / 吳燦銘, 胡昭民著',
+            detail: [ '公館分館6F新類號專區', '312.32C 8866-2', 'BM0736996', '可外借' ]
+        },
+        {
+            href: 'http://www.lib.ntnu.edu.tw/holding/doQuickSearch.jsp?action=view&param=%2Fsearch*cht%3F%2Ft%257Bu6F14%257D%257Bu7B97%257D%257Bu6CD5%257D%2Ft%257B214852%257D%257B21502c%257D%257B21472a%257D%2F1%252C24%252C31%252CB%2Fframeset%26FF%3Dt%7B214852%7D%7B21502c%7D%7B21472a%7D%263%252C%252C4',
+            name: '圖說演算法 : 使用Python / 吳燦銘, 胡昭民著',
+            detail: [ '總館6F新類號專區', '312.32P999 8866 2018', 'BM0757623', '到期 09-02-23' ]
+        },
+        {
+            href: 'http://www.lib.ntnu.edu.tw/holding/doQuickSearch.jsp?action=view&param=%2Fsearch*cht%3F%2Ft%257Bu6F14%257D%257Bu7B97%257D%257Bu6CD5%257D%2Ft%257B214852%257D%257B21502c%257D%257B21472a%257D%2F1%252C24%252C31%252CB%2Fframeset%26FF%3Dt%7B214852%7D%7B21502c%7D%7B21472a%7D%264%252C%252C4',
+            name: '演算法 / Anany V. Levitin原著 ; 莊承翃譯.',
+            detail: [ '公館分館', '000.5131 117', 'BM0474069', '可外借' ]
+        }
+    ]
+    ```
+
+    '''
+    url = json.loads(urlObj).get('url')
+    # print(url)
+    result = crewl.findBook(url)
+    return result
+
+
+@app.get("/correction")
+def correction(wrong_text: str):
+    return crewl.correctWords(wrong_text)
